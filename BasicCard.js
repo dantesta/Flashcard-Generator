bbcaac3083d1f00d41de3e2f084c9b1a103d5127
Dan Testa <dantesta@Dans-MacBook-Pro-3.local>
2017-10-13 21:29:48 -0400

    BasicCard working

--- BasicCard.js
+++ BasicCard.js
@@ -1,1 +1,13 @@
+function BasicCard(front, back) {
+
+	if (!(this instanceof BasicCard)) {
+
+		return new BasicCard(front, back);
+	}
+
+	this.front = front;
+	this.back = back;
+};
+
+
 module.exports = BasicCard;
--- CLI.js
+++ CLI.js
@@ -1,2 +1,16 @@
 var BasicCard = require("./BasicCard.js");
-var ClozeCard = require("./ClozeCard.js");
+// var ClozeCard = require("./ClozeCard.js");
+
+
+var questions = {
+
+	Question1: BasicCard(
+		"What is the third largest planet in the solar system??",
+		"Uranus"
+	),
+};
+
+
+console.log("\n================\n");
+console.log(questions.Question1);
+console.log("\n================\n");
